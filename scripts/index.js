8acfa6ee8e7917903c5849cdc24fd982f8548846
Popov Ivan <next.wot@gmail.com>
2024-02-12 20:30:07 +0500

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -11,21 +11,24 @@ const cardList = content.querySelector('.places__list');
  */
 function renderCard(cardData, onDelete) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const image = cardElement.querySelector('.card__image');
+
+    image.src = cardData.link;
+    image.alt = cardData.name;
 
     cardElement.querySelector('.card__title').textContent = cardData.name;
-    cardElement.querySelector('.card__image').src = cardData.link;
-    cardElement.querySelector('.card__delete-button').onclick = () => onDelete(cardElement);
+    cardElement.querySelector('.card__delete-button').onclick = onDelete;
 
-    cardList.append(cardElement)
+    return cardElement;
 }
 
 /**
  * Функция удаления карточки.
- * @param element HTML-элемент.
+ * @param event Cобытие клика.
  */
-function deleteCard (element) {
-    element.remove();
+function deleteCard (event) {
+    event.target.closest('.card').remove();
 }
 
 // Выводим карточки на страницу
-cardList.append(initialCards.map((data) => renderCard(data, deleteCard)))
+cardList.append(...initialCards.map((data) => renderCard(data, deleteCard)))
