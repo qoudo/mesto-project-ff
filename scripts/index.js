936486aa9541ac50c9e9d300e6c98b2ffa0990ff
Popov Ivan <next.wot@gmail.com>
2024-02-12 20:18:30 +0500

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate =  document.querySelector('#card-template').content;
+// DOM узлы
+const content = document.querySelector('.content');
+const cardList = content.querySelector('.places__list');
 
-// @todo: DOM узлы
+/**
+ * Функция создания карточки.
+ * @param cardData Данные карточки.
+ * @param onDelete Обработчик удаления карточки.
+ */
+function renderCard(cardData, onDelete) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция создания карточки
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__delete-button').onclick = () => onDelete(cardElement);
 
-// @todo: Функция удаления карточки
+    cardList.append(cardElement)
+}
 
-// @todo: Вывести карточки на страницу
+/**
+ * Функция удаления карточки.
+ * @param element HTML-элемент.
+ */
+function deleteCard (element) {
+    element.remove();
+}
+
+// Выводим карточки на страницу
+cardList.append(initialCards.map((data) => renderCard(data, deleteCard)))
