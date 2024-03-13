6d231b768ad031fced8fdd1ad8e5133292d3e5e9
Popov Ivan <next.wot@gmail.com>
2024-03-13 12:31:08 +0500

    add config webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './pages/index.css';
+import { INITIAL_CARDS } from './scripts/constants';
+
+const cardList = content.querySelector('.places__list');
+
+// Выводим карточки на страницу
+cardList.append(...INITIAL_CARDS.map((data) => renderCard(data, deleteCard)))
--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,26 +1,32 @@
-export const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
-];
+import { CARD_SELECTOR } from './constants';
+
+// Темплейт карточки
+const cardTemplate =  document.querySelector('#card-template').content;
+// DOM узлы
+const content = document.querySelector('.content');
+
+/**
+ * Функция создания карточки.
+ * @param cardData Данные карточки.
+ * @param onDelete Обработчик удаления карточки.
+ */
+function renderCard(cardData, onDelete) {
+    const cardElement = cardTemplate.querySelector(cardSelector).cloneNode(true);
+    const image = cardElement.querySelector('.card__image');
+
+    image.src = cardData.link;
+    image.alt = cardData.name;
+
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__delete-button').onclick = onDelete;
+
+    return cardElement;
+}
+
+/**
+ * Функция удаления карточки.
+ * @param event Cобытие клика.
+ */
+function deleteCard (event) {
+    event.target.closest(CARD_SELECTOR).remove();
+}
