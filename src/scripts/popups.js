860b65d019db9d7ae391a37df36c3294a750c50d
Popov Ivan <next.wot@gmail.com>
2024-03-14 00:21:48 +0500

    fix

--- src/scripts/popups.js
+++ src/scripts/popups.js
@@ -1,16 +1,15 @@
-const POPUPS = {
+const popups = {
   edit: document.querySelector('.popup_type_edit'),
   addCard: document.querySelector('.popup_type_new-card'),
   gallery: document.querySelector('.popup_type_image')
 }
-
-const CURRENT_POPUP_SELECTOR = 'popup_is-opened'
+const currentPopupSelector = 'popup_is-opened'
 
 /**
  * Закрытие popup'а.
  */
 export function closePopup () {
-  document.querySelector(`.${CURRENT_POPUP_SELECTOR}`).classList.remove(CURRENT_POPUP_SELECTOR)
+  document.querySelector(`.${currentPopupSelector}`).classList.remove(currentPopupSelector)
 }
 
 /**
@@ -26,7 +25,7 @@ export function handleKeyDownEsc (event) {
  * @param {Event} event Данные события.
  */
 export function handleClickOverlay (event) {
-  if (event.target.classList.contains(CURRENT_POPUP_SELECTOR)) {
+  if (event.target.classList.contains(currentPopupSelector)) {
     closePopup()
     event.target.removeEventListener('click', handleClickOverlay)
   }
@@ -38,12 +37,12 @@ export function handleClickOverlay (event) {
  * @param {function} [callback] Функция callback.
  */
 export function openPopup (name, callback) {
-  POPUPS[name].classList.add(CURRENT_POPUP_SELECTOR)
+  popups[name].classList.add(currentPopupSelector)
 
   callback && callback()
 
   // Навешиваем обработчики закрытия модального окна
-  POPUPS[name].addEventListener('click', handleClickOverlay)
+  popups[name].addEventListener('click', handleClickOverlay)
   document.addEventListener('keydown', handleKeyDownEsc, { once: true })
-  POPUPS[name].querySelector('.popup__close').addEventListener('click', closePopup, { once: true })
+  popups[name].querySelector('.popup__close').addEventListener('click', closePopup, { once: true })
 }
